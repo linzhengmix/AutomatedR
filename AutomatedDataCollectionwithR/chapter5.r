6c7c31b9dc490b6422515f349cd9fab5128f8ce0
Linzheng <759967211@qq.com>
2019-01-27 23:49:37 +0800

    i
    add new file and script

---
+++ AutomatedDataCollectionwithR/chapter5.r
@@ -0,0 +1,9 @@
+
+# RCurl包是R语言与网络服务器交互常用的包之一。
+# RCurl组件支持的协议种类很多,包括：
+# > curlVersion()$protocols
+#  [1] "dict"   "file"   "ftp"    "ftps"   "gopher" "http"   "https"  "imap"   "imaps" 
+# [10] "ldap"   "ldaps"  "pop3"   "pop3s"  "rtsp"   "smb"    "smbs"   "smtp"   "smtps" 
+# [19] "telnet" "tftp"  
+
+
